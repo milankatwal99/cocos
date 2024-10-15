bc78ee8fd1cbab77d6250e835224d2b028cd7318
Milan Katwal <milankatwal@Milans-Laptop.local>
2024-10-15 08:47:00 +0545

    Initial commit

---
+++ Example/Pods/Target Support Files/milan2056/milan2056-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_milan2056 : NSObject
+@end
+@implementation PodsDummy_milan2056
+@end
--- Example/milan2056/ViewController.swift
+++ Example/milan2056/ViewController.swift
@@ -1,18 +1,26 @@
 //
 //  ViewController.swift
-//  milan2056
+//  MyCocoapodsLibrary
 //
-//  Created by milankatwal99 on 10/15/2024.
-//  Copyright (c) 2024 milankatwal99. All rights reserved.
+//  Created by iamnaz on 02/21/2020.
+//  Copyright (c) 2020 iamnaz. All rights reserved.
 //
 
 import UIKit
+import MyCocoapodsLibrary
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let log = Logger()
+        log.printLog()
+        let frameworkBundle = Bundle(for: Logger.self)
+        let path = frameworkBundle.path(forResource: "Resources", ofType: "bundle")
+        let resourcesBundle = Bundle(url: URL(fileURLWithPath: path!))
+        let image = UIImage(named: "flower.jpg", in: resourcesBundle, compatibleWith: nil)
+        
+        print(image)
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,4 +29,3 @@ class ViewController: UIViewController {
     }
 
 }
-
